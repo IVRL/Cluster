50d224d74264a21b689f993dd097fc09e9e6b794
groenqui <peter.groenqui@gmail.com>
2022-02-24 15:08:38 +0100

    Moved to RunAI folder

---
+++ RunAI/images/build_cu11.sh
@@ -0,0 +1,37 @@
+
+
+CU_IMAGE=nvidia/cuda:11.3.1-cudnn8-devel-ubuntu20.04
+BRANCH=cuda113
+BRANCH_PUBLISH=cuda11
+
+docker pull $CU_IMAGE
+
+docker build lab-base --file lab-base/Dockerfile \
+	--build-arg BASE_IMG=$CU_IMAGE \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-base:$BRANCH \
+	--network="host"
+
+docker build lab-pytorch \
+	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-base:$BRANCH \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch:$BRANCH \
+	--network="host"
+
+docker build lab-python-ml \
+	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch:$BRANCH \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-python-ml:$BRANCH \
+	--network="host"
+
+
+# docker run -it ic-registry.epfl.ch/cvlab/lis/lab-python-ml:$BRANCH /bin/bash
+
+#docker login ic-registry.epfl.ch
+
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch:$BRANCH
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:$BRANCH
+
+# docker tag ic-registry.epfl.ch/cvlab/lis/lab-pytorch:$BRANCH ic-registry.epfl.ch/cvlab/lis/lab-pytorch:$BRANCH_PUBLISH
+# docker tag ic-registry.epfl.ch/cvlab/lis/lab-python-ml:$BRANCH ic-registry.epfl.ch/cvlab/lis/lab-python-ml:$BRANCH_PUBLISH
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch:$BRANCH_PUBLISH
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:$BRANCH_PUBLISH
+
+#rm ~/.docker/config.json
